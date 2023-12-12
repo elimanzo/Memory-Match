7d979f63f1a337d714e06d0d4256ceb3a9f306c5
Eli Manzo <96manzo.eli@gmail.com>
2023-12-12 00:04:32 -0800

    Adding Reducer and Initialized state

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { useReducer } from 'react';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'make-a-guess':
+      return { ...state, guesses: state.guesses + 1 };
+    case 'reset-guess':
+      return state;
+    case 'reset-game':
+      return state;
+  }
+  return state;
+}
+
+function getInitialState() {
+  return {
+    board: ['a', 'b', 'c', 'a', 'b', 'c'],
+    revealed: [],
+    guesses: 0
+  };
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, undefined, getInitialState);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button
+        title='Guess'
+        onPress={() => dispatch({ type: 'make-a-guess' })}
+      />
+      <Text>{JSON.stringify(state, null, 2)}</Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -15,6 +41,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
